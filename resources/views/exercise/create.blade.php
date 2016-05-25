abc6aaff0fb668028b3e1cce8043b3bdb26e8c87
febju <felix.bjugg@gmail.com>
2016-05-25 16:59:04 +0300

    nappi järjestyksessä

--- resources/views/exercise/create.blade.php
+++ resources/views/exercise/create.blade.php
@@ -9,14 +9,10 @@
 {{ Form::open(array('action'=> array('ExerciseController@store'), 'method'=>'POST')) }}
 <form class="form-horizontal">
 	<div class="form-group">
-		<div class="col-sm-12">
-			{!! Form::text('name', null, array('required', 'class'=>'form-control', 'placeholder'=>'Harjoituksen nimi')) !!}
-		</div>
+		{!! Form::text('name', null, array('required', 'class'=>'form-control', 'placeholder'=>'Harjoituksen nimi')) !!}
 	</div>	
 	<div class="form-group">
-		<div class="col-sm-12">
-			{!! Form::select('topic_id', $topic_list, null, ['class' => 'form-control']) !!}
-		</div>
+		{!! Form::select('topic_id', $topic_list, null, ['class' => 'form-control']) !!}
 	</div>
 	{!! Form::submit('Lisää',['class' => 'btn btn-primary']) !!}
 	{!! Form::close() !!}
