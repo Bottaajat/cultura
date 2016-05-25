5d998a04a71a797a8d8ec37c8d20da0ae3076126
febju <felix.bjugg@gmail.com>
2016-05-25 22:05:30 +0300

    aakkosproto2

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -15,6 +15,14 @@
     padding: 10px;
     border: 1px solid #aaaaaa;
 }
+.alphabet-area {
+    width: 100%; 
+    height: 30px;
+    margin: 15px;
+    padding: 10px;
+    border: 1px solid #aaaaaa;
+}
+
 </style>
 
 <div class="panel panel-default">
@@ -22,8 +30,7 @@
 		<div class="panel-title">{{$task->type}}</div>
 	</div>
 	<div class="panel-body">
-		<div id="dropboxes">
-		
+		<div>
 			<div class="droptarget" ondrop="drop(event)" ondragover="allowDrop(event)" id="droptarget-a" data-target="a">
 			
 			</div>
@@ -36,15 +43,12 @@
 			
 			</div>
 		</div>
-		
-		<div id="alphabet">
+		<div>
 			<p class="draggable" ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="draggable-a">&#1072;</p>
 			<p class="draggable" ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="draggable-b">&#1073;</p>
 			<p class="draggable" ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true" id="draggable-c">&#1074;</p>
 		</div>
 		
-		<br>
-		<input class="btn btn-default" type="submit" value="Palauta">
 	</div>
 </div>
 
@@ -64,7 +68,11 @@ function drop(event) {
     var data = event.dataTransfer.getData("Text");
 	var drag_id = data.split("-");
 	var drop_id = (event.target.id).split("-");
-	if (drag_id[1] == drop_id[1]) {event.target.appendChild(document.getElementById(data));}
+	if (drag_id[1] == drop_id[1]) {
+		event.target.appendChild(document.getElementById(data));
+		var element = event.target.id;
+		document.getElementById(element).style.border = "1px solid #00FF00";
+	}
 }
 
 </script>
