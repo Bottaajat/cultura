27b1dbe00b9375809fd34bef02c1a05716ab249f
febju <felix.bjugg@gmail.com>
2016-05-26 15:52:24 +0300

    päivitetyt tehtävät

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -41,6 +41,23 @@
 	<div class="panel-body">
 		@foreach($task->exercise->materials as $material)
 			@if($material->type == "image")
+			<div id="draggable-{{$material->id}}" class="draggable" ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true">	
+				<div class="col-sm-2">
+					<div class="thumbnail">
+					{{$material->label}}
+					</div>
+				</div>
+			</div>
+			@endif
+			@if($material->type == "sound")
+			<div class="col-sm-2">
+					<div id="droptarget-{{$material->id}}" src="{{$material->src}}" data-target="{{$material->id}}" class="thumbnail"  style="height: 70px; width: 60px" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+					
+			</div>
+			@endif
+		@endforeach
+		@foreach($task->exercise->materials as $material)
+			@if($material->type == "image")
 			<div class="col-sm-2">
 				<div class="thumbnail">
 				<img id="droptarget-{{$material->id}}" src="{{$material->src}}" data-target="{{$material->id}}" class="droptarget" ondrop="drop(event)" ondragover="allowDrop(event)" height="64" width="64">
@@ -57,25 +74,6 @@
 			</div>
 			@endif
 		@endforeach
-		@foreach($task->exercise->materials as $material)
-			@if($material->type == "image")
-			<div id="draggable-{{$material->id}}" class="draggable" ondragstart="dragStart(event)" ondrag="dragging(event)" draggable="true">	
-				<div class="col-sm-2">
-					<div class="thumbnail">
-					{{$material->label}}
-					</div>
-				</div>
-			</div>
-			@endif
-			@if($material->type == "sound")
-			<div class="col-sm-2">
-				<div class="thumbnail">
-					<div id="droptarget-{{$material->id}}" src="{{$material->src}}" data-target="{{$material->id}}" class="droptarget" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-				</div>
-			</div>
-			@endif
-		@endforeach
-	</div>
 </div>
 
 @stop
