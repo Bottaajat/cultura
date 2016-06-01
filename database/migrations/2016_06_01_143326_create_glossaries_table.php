98f3bd056857d2c4d58cd1f514417722c66bc5e1
Eero Sääksvuori <esaaksvu@cs.helsinki.fi>
2016-06-01 17:48:30 +0300

    sanastotaulu

---
+++ database/migrations/2016_06_01_143326_create_glossaries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGlossariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('glossaries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('glossaries');
+    }
+}
