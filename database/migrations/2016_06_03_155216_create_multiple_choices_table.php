95313226469811ce1df1156538bff56de71688f4
mihassin <marko.hassinen@helsinki.fi>
2016-06-03 18:55:27 +0300

    multichoice

--- database/migrations/2016_06_03_155216_create_multiple_choices_table.php
+++ database/migrations/2016_06_03_155216_create_multiple_choices_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAssignmentsTable extends Migration
+class CreateMultipleChoicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,11 @@ class CreateAssignmentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('assignments', function (Blueprint $table) {
+        Schema::create('multiple_choices', function (Blueprint $table) {
             $table->increments('id');
-			      $table->string('content', 1000);
+            $table->string('question');
+            $table->string('choices', 1000);
+            $table->string('solution');
             $table->integer('task_id')->unsigned();
             $table->foreign('task_id')->references('id')->on('tasks');
             $table->timestamps();
@@ -28,6 +30,6 @@ class CreateAssignmentsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('assignments');
+        Schema::drop('multiple_choices');
     }
 }
