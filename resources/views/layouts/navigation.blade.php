b1ecec33aa576c2cb7896e7e3fe4e5f2f2aa0980
mihassin <marko.hassinen@helsinki.fi>
2016-06-08 19:45:29 +0300

    käyttäjien listaus merkki

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,7 @@
 					<ul class="dropdown-menu">
 						<li> <a href="{{ url('/material/') }}"><i class="fa fa-btn fa-align-justify"></i> Materiaalin listaus</a> </li>
 						<li> <a href="{{ url('/exercise/') }}"><i class="fa fa-btn fa-align-justify"></i> Harjoitusten listaus</a> </li>
-						<li> <a href="{{ URL::to('/user/') }}">Käyttäjien listaus</a> </li>
+						<li> <a href="{{ URL::to('/user/') }}"><i class="fa fa-btn fa-align-justify"></i> Käyttäjien listaus</a> </li>
 						<li role="separator" class="divider"></li>
 						<li> <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Kirjaudu ulos</a> </li>
 					</ul>
