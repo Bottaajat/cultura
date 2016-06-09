19ce7334d57edf4031a4c20fce5b0caca6d53da3
Eero Sääksvuori <esaaksvu@cs.helsinki.fi>
2016-06-10 02:05:12 +0300

    Multichoice paranneltu

--- public/js/multipleChoice.js
+++ public/js/multipleChoice.js
@@ -1,20 +1,52 @@
 function checkSolutions() {
-  var corrects = 0;
-  var questions = document.getElementsByClassName("radio");
+
+  var questions = document.getElementsByName("question");
   for(var i = 0; i <= questions.length; i++) {
     if(questions[i]) {
-      var radio = questions[i].getElementsByTagName("input")[0];
-      if(radio.checked === true) {
-        if(radio.getAttribute('value') === 0) {
-          radio.parentNode.parentNode.style.backgroundColor = "green";
-          corrects = corrects + 1;
-        } else {
-          radio.parentNode.parentNode.style.backgroundColor = "red";
+      var options = questions[i].getElementsByClassName("option");
+      var checked = 0;
+      var correctCount = 0;
+      
+      for(var j = 0; j <= options.length; j++) {
+        if(options[j]) {
+          var option = options[j];
+          option.parentNode.parentNode.style.backgroundColor = "white";
+          if (option.checked === true)
+            checked = checked + 1;
+          if (option.getAttribute('value')) 
+            correctCount = correctCount + 1;
         }
-      } else {
-        radio.parentNode.parentNode.style.backgroundColor = "white";
+      }
+      if (checked < correctCount) {
+        $("#tooFew").modal();
+        return;
+      } else if (checked > correctCount) {
+        $("#tooMany").modal();
+        return;
       }
     }
   }
-  if (corrects === 4) $("#success").modal() 
+  
+  var options = document.getElementsByClassName("option");
+  var corrects = 0;
+  var correctCount = 0;
+    
+  for(var j = 0; j <= options.length; j++) {
+    if(options[j]) {
+      var option = options[j];
+      option.parentNode.parentNode.style.backgroundColor = "white";
+      if(option.getAttribute('value')) {
+        correctCount = correctCount + 1;
+        if(option.checked === true) {
+          option.parentNode.parentNode.style.backgroundColor = "green";
+          corrects = corrects + 1;
+        } 
+      } else if (option.checked === true) {
+        option.parentNode.parentNode.style.backgroundColor = "red";
+      } 
+    }
+  }
+
+  if (corrects === correctCount) $("#success").modal();
+
 }
--- resources/views/multipleChoice/show.blade.php
+++ resources/views/multipleChoice/show.blade.php
@@ -2,22 +2,24 @@
   {!! Html::script("/js/multipleChoice.js") !!}
 @stop
 
-
 <form onSubmit="checkSolutions(); return false;">
+  
     @foreach($task->multiplechoises as $mc)
     <div name="question" class="form-group">
         <label>{!! $mc->id . ". " . $mc->question !!}</label>
-
-
+        
         @foreach($mc->randomOrderChoices() as $key => $choice)
-        <div class="radio">
-          <label>
-           <input type="radio" name="optionsRadios-{{$mc->id}}" id="optionsRadios-{{$mc->id}}-{{$key}}" value="{{ strcmp($choice, $mc->solution) }}">
-            {{ $choice }}
-          </label>
+        <div class="question">
+          <p>
+            @if($mc->getSolutionCount() == 1) 
+              <input type="radio" autocomplete="off" class="option" name="optionsRadios-{!!$mc->id!!}" id="optionsRadios-{!!$mc->id!!}-{!!$key!!}" value="{!! $mc->checkSolution($choice) !!}">
+            @else
+              <input type="checkbox" autocomplete="off" class="option" name="optionsRadios-{!!$mc->id!!}" id="optionsRadios-{!!$mc->id!!}-{!!$key!!}" value="{!! $mc->checkSolution($choice) !!}">
+            @endif
+            {!! $choice !!}
+          </p>
         </div>
         @endforeach
-
 
     </div>
     @endforeach
@@ -26,3 +28,30 @@
     	<button class="btn btn-success" type="submit">Tarkista</button>
     </div>
 </form>
+
+<div id="tooMany" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h4 class="modal-title">Valitsit liian monta vaihtoehtoa.</h4>
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Sulje</button>
+	  </div>
+		</div>
+	</div>
+</div>
+
+
+<div id="tooFew" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h4 class="modal-title">Et valinnut kaikkia kohtia!</h4>
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Sulje</button>
+		  </div>
+		</div>
+	</div>
+</div>
