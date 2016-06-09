f539cd106e96033f17c0841a99805307cb4fb4c9
Eero Sääksvuori <esaaksvu@cs.helsinki.fi>
2016-06-09 21:03:29 +0300

    glossary pois emptyt ja check

--- app/Http/Controllers/GlossaryController.php
+++ app/Http/Controllers/GlossaryController.php
@@ -10,27 +10,45 @@ use App\Glossary;
 
 class GlossaryController extends Controller
 {
+
+  private function clearEmptyLines($str) {
+    return preg_replace('/^[ \t]*[\r\n]+/m', '', $str);
+  }
+  
+  private function checkLines($rus,$fin) {
+    $countRus = count(stringToArray($rus));
+    $countFin =  count(stringToArray($fin));
+    return ($countRus == $countFin);
+  }
+
+
   public function __construct() {
     $this->middleware('auth');
   }
 
   public function store(Request $request) {
     $glossary = new Glossary();
-    $glossary->rus = $request->input('rus');
-    $glossary->fin = $request->input('fin');
+    $glossary->rus = $this->clearEmptyLines($request->input('rus'));
+    $glossary->fin = $this->clearEmptyLines($request->input('fin'));
     $glossary->material()->associate($request->input('material_id'));
-
-    $glossary->save();
-    return back()->with('success', 'Sanasto luotu!');
+    if ($this->checkLines($glossary->rus,$glossary->fin)) {
+      $glossary->save();
+      return back()->with('success', 'Sanasto päivitetty!');
+    } else {
+      return back()->withErrors("Rivit eivät täsmää");
+    }
   }
 
   public function update(Request $request, $id) {
     $glossary = Glossary::find($id);
-    $glossary->rus = $request->input('rus');
-    $glossary->fin = $request->input('fin');
-
-    $glossary->save();
-    return back()->with('success', 'Sanasto päivitetty!');
+    $glossary->rus = $this->clearEmptyLines($request->input('rus'));
+    $glossary->fin = $this->clearEmptyLines($request->input('fin'));
+    if ($this->checkLines($glossary->rus,$glossary->fin)) {
+      $glossary->save();
+      return back()->with('success', 'Sanasto päivitetty!');
+    } else {
+      return back()->withErrors("Rivit eivät täsmää");
+    }
   }
 
   public function destroy($id) {
@@ -38,5 +56,6 @@ class GlossaryController extends Controller
     $glossary->delete();
     return back()->with('success', 'Sanasto poistettu!');
   }
+  
 
 }
