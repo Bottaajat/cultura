56f579d532bb8543afea0afa76130d9ab9a55ddb
mihassin <marko.hassinen@helsinki.fi>
2016-06-13 15:44:01 +0300

    don't judge me brah

--- resources/views/topic/index.blade.php
+++ resources/views/topic/index.blade.php
@@ -16,41 +16,31 @@
   </h1>
 </div>
 
-<div class="panel-group" id="accordion">
-@foreach($topics as $topic)
-
-<div class="panel panel-default">
-
-{{-- Header (Topic) --}}
-<div class="panel-heading"  data-toggle="collapse" data-parent="#accordion" data-target='{{"#collapse".$topic->id}}'>
-  <h4 class="panel-title">
-    <a id="menu-{{$topic->id}}-toggle" >
-      {{$topic->name}}
-      <i id=panelarrow class="glyphicon glyphicon-triangle-left pull-right"></i>
-    </a>
-  </h4>
-</div>
-
-{{-- Header (Exercises) --}}
-<div id='{{"collapse".$topic->id}}' class="panel-collapse collapse">
-  <div class="list-group">
-    @foreach($topic->exercises as $exercise)
-    <div class="container-fluid list-group-item">
-      <div class="row">
-        <div class="pull-left">
-          <a class="list-group-item" style="margin-left:10px" href="{{URL::to($topic->name.'/'.$exercise->name)}}"> {{$exercise->name}} </a>
+<div class="row">
+  @foreach($topics as $topic)
+    <div class="col-xs-6 col-md-6">
+      <div class="panel-group">
+        <div class="panel panel-default">
+          <div class="panel-heading"  data-toggle="collapse" data-target="{!! '#collapseTopic' . $topic->id !!}">
+            <h4 class="panel-title">
+              <div id="{!! 'menu-' . $topic->id . '-toggle'!!}}">
+                 {!! $topic->name !!}
+                <i id=panelarrow class="glyphicon glyphicon-triangle-bottom pull-right"></i>
+              </div>
+            </h4>
+          </div>
+          <div id="{!! 'collapseTopic' . $topic->id !!}" class="panel-collapse collapse in">
+            <div class="list-group">
+              @foreach($topic->exercises as $exercise)
+                <a href="" class="list-group-item"> {{ $exercise->name }} </a>
+              @endforeach
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    @endforeach
-  </div>
+  @endforeach
 </div>
-
-</div>
-
-@endforeach
-</div>
-
 @endif
 
 @stop
