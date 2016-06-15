ad24cca7fe0d04f03197ef86495c78690e1a6143
mihassin <marko.hassinen@helsinki.fi>
2016-06-15 17:45:13 +0300

    navi muoto

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,24 +20,34 @@
         <li class="dropdown dropdown-submenu">
         	<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-btn fa-cog"></i> Menu</a>
 					<ul class="dropdown-menu">
-						<li> <a href="{{ URL::to('/user/') }}"><i class="fa fa-btn fa-users"></i> Käyttäjien listaus</a> </li>
 						<li> <a href="{{ url('/exercise/') }}"><i class="fa fa-btn fa-book"></i> Harjoitusten listaus</a> </li>
 						<li> <a href="{{ url('/task/') }}"><i class="fa fa-btn fa-pencil"></i> Tehtävien listaus</a> </li>
 						@if(Auth::check())
 						<li role="separator" class="divider"></li>
-						<li> <a href="{{ url('school/')}}"><i class="fa fa-btn fa-building"></i> Koulut</a> </li>
 						<li> <a href="{{ url('/material/') }}"><i class="fa fa-btn fa-file"></i> Materiaalin listaus</a> </li>
+						@endif
 						<li role="separator" class="divider"></li>
-						<li> <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Kirjaudu ulos</a> </li>
+						<li> <a href="{{ URL::to('/user/') }}"><i class="fa fa-btn fa-users"></i> Käyttäjien listaus</a> </li>
+						<li> <a href="{{ url('school/')}}"><i class="fa fa-btn fa-building"></i> Koulut</a> </li>
+					</ul>
+        </li>
+
+
+				<li class="dropdown dropdown-submenu">
+        	<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-btn fa-power-off"></i></a>
+					<ul class="dropdown-menu">
+						@if(Auth::guest())
+							<li><a href="{{ url('/register') }}"><i class="fa fa-btn fa-user"></i> Rekisteröidy</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="{{ url('/login') }}"><i class="fa fa-btn fa-sign-in"></i> Kirjaudu sisään</a></li>
+						@else
+							<li><a href="{{ action('UserController@show', ['id' => Auth::user()->id]) }}"><i class="fa fa-btn fa-user"></i> {!! Auth::user()->name() !!}</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Kirjaudu ulos</a> </li>
 						@endif
 					</ul>
         </li>
 
-				@if(Auth::guest())
-					<li class="pull-right"><a href="{{ url('/login') }}"><i class="fa fa-btn fa-sign-in"></i> Kirjaudu sisään</a></li>
-				@else
-					<li class="pull-right"><a href="{{ action('UserController@show', ['id' => Auth::user()->id]) }}"><i class="fa fa-btn fa-user"></i> {!! Auth::user()->name() !!}</a></li>
-				@endif
 			</ul>
 		</div>
 
