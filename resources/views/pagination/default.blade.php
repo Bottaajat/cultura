0889dc1acb4e02258b63ad4a4fec6934999e604c
Eero Sääksvuori <esaaksvu@cs.helsinki.fi>
2016-06-23 01:12:34 +0300

    paginaatio

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -1,50 +1,37 @@
 @if ($paginator->lastPage() > 1)
 
   <ul class="pagination">
+      
+      <li class="{!! ($paginator->currentPage() == 1) ? 'disabled' : '' !!}">
+        <a href="{!! $paginator->url(1) !!}">
+          <<
+        </a>
+      </li>
+      
       <li class="{!! ($paginator->currentPage() == 1) ? ' disabled' : '' !!}">
           <a href="{!! $paginator->previousPageUrl() !!}">
             Edellinen
           </a>
       </li>
       
-      <li class="{!! ($paginator->currentPage() == 1) ? ' active' : '' !!}">
-        <a href="{!! $paginator->url(1) !!}">
-          {!! ($paginator->currentPage() == 1) ? ' 1' : '<<' !!}
-        </a>
+       <li class="active">
+          <a href="">
+            {!! $paginator->currentPage() !!}
+          </a>
       </li>
       
-      @for (
-          $i = ($paginator->currentPage() < 3 ? 
-                  2: 
-                  ($paginator->lastPage()-$paginator->currentPage() > 1 ?
-                    $paginator->currentPage()-1 :
-                    $paginator->lastPage() - 4));
-                
-          $i <= ($paginator->currentPage() < 3 ?
-                  5:
-                  ($paginator->lastPage()-$paginator->currentPage() > 1 ?
-                    $paginator->currentPage()+2: 
-                    $paginator->lastPage()-1));
-          $i++)
-
-          <li class="{!! ($paginator->currentPage() == $i) ? ' active' : '' !!}">
-              <a href="{!! $paginator->url($i) !!}">{!! $i !!}</a>
-          </li>
-      @endfor
-      
-      
-      <li class="{!! ($paginator->currentPage() == $paginator->lastPage()) ? ' active' : '' !!}">
-        <a href="{!! $paginator->url($paginator->lastPage()) !!}">
-          {{($paginator->currentPage() == $paginator->lastPage()) ? $paginator->lastPage() : '>>' }}
-        </a>
-      </li>
-
 
       <li class="{!! $paginator->hasMorePages()  ? '' : ' disabled' !!}">
           <a href="{!! $paginator->nextPageUrl() !!}">
             Seuraava
           </a>
       </li>
+      <li class="{!! ($paginator->currentPage() == $paginator->lastPage()) ? 'disabled' : '' !!}">
+        <a href="{!! $paginator->url($paginator->lastPage()) !!}">
+          >>
+        </a>
+      </li>
+
   </ul>
 
 @endif
