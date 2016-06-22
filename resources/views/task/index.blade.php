8d985ecaad88512ca4feb55638029aa6cec2c8fd
febju <felix.bjugg@gmail.com>
2016-06-23 00:23:16 +0300

    make pagination great again

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -5,11 +5,27 @@
   <h1>Tehtävät </h1>
 </div>
 
-@if(Auth::check())
-  <div class="createbuttondiv">
-    @include('task.create')
-  </div>
-@endif
+<div class="createbuttondiv">
+	  <div class="row">
+		<div class="col-xs-6 .col-sm-4 pull-right">
+			@if(Auth::check())
+				@include('task.create')
+			@endif
+	   </div>
+
+		<div class="col-xs-6 .col-sm-4">
+			{!! Form::open(array('action'=>array('TaskController@index'), 'method'=>'get')) !!}
+				<div class="input-group">
+					{!! Form::text('search', (isset($search) ? $search : ''), array('class' => ' form-control', 'placeholder'=>'Haku')) !!}
+					<span class="input-group-btn">
+						{!! Form::submit('Hae', array('class' => 'btn btn-primary')) !!}
+					</span>
+				</div>
+			{!! Form::close() !!}
+		</div>
+	</div>
+</div>
+
 <div class="table-responsive">
 <table class="table table-bordered table-hover">
 
@@ -62,4 +78,9 @@
 
 </table>
 </div>
+
+<div class="paginationdiv">
+  @include('pagination.default', ['paginator' => $tasks])
+</div>
+
 @stop
