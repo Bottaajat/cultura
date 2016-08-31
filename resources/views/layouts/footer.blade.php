cd28dec1f8884c9af4c1ab17565bf4a37bd5434a
mihassin <marko.hassinen@helsinki.fi>
2016-08-31 16:01:41 +0300

    maininta neasta ja projektin maili kuntoon

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,11 +3,12 @@
     <div class="contact">
      <a class="footer-link" href="http://www.akkunavenajalle.fi">Akkuna venäjälle</a>
      <a class="footer-link" href="https://www.youtube.com/channel/UCWtlywbFLyl8Ey-IavTZ2HQ"><i class="fa fa-youtube-play" aria-hidden="true"></i> YouTube kanava </a>
-     <a class="footer-link" href="mailto:venaja.verkkokurssi@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i> Uusia videoita?</a>
+     <a class="footer-link" href="mailto:venajanverkkokurssi@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i> Uusia videoita?</a>
     </div>
 
   <div class="shameless-self-promotion">
     <p>Sivuston suunnitteli ja kokosi <a class="footer-link" href="https://github.com/Bottaajat/cultura">https://github.com/Bottaajat/cultura</a></p>
+    <p>Sivuston taustakuvan suunnitteli <a class="footer-link" href="mailto:nea.raivio@gmail.com">Nea Raivio</a> </p>
   </div>
  </div>
 </div>
